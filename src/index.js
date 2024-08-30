95b32ee236b2eace1c323ff83e1adbcb1197919e
srj-17 <saugatrijal17@gmail.com>
2024-08-30 19:51:01 +0545

    Do Initial logical setup of project
    We have created 3 types of objects:
    - User (has projects)
    - Projects (have todos)
    - Todos
    They communicate with each other.
    User creates new projects within itself by creating
    new objects. Projects create todos the same way.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import t from './project'
+import project from './project';
+
+const user = (function () {
+    let projects = [];
+    function addProject(name, priority, duedate) {
+        let newProject = new project(name, priority, duedate);
+        projects.push(newProject);
+    };
+
+    return {addProject, projects};
+}) ();
+
+user.addProject("best world project", 1, "2011-10-1");
--- src/project.js
+++ src/project.js
@@ -1,3 +1,17 @@
 import todo from './todo';
-let t;
-export default t = new todo();
+
+class Project {
+    constructor(name, priority, dueDate) {
+        this.name = name;
+        this.priority = priority;
+        this.dueDate = dueDate ? new Date(dueDate) : new Date();
+        this.todo = [];
+    }
+    
+    // we're trusting valid todo to be provided. should we? 
+    addTodo(todo) {
+        this.todo.push(todo);
+    }
+}
+
+export default Project;
