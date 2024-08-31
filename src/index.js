002e6837a65710332a8486f38ec3d83ef35ca978
srj-17 <saugatrijal17@gmail.com>
2024-08-31 14:54:46 +0545

    Add a default project for users
    So that they can add projects directly without any
    setup, when they do that, its added as todo of default
    project.
    So, the structure of todo is entirely the same.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import project from './project';
+import todo from './todo';
 
 const user = (function () {
     let projects = [];
+    
+    // if todos added at random (outside of projects), they're added to default project
+    // it has no duedate since they're decided by the tasks
+    // let defaultProject = (function () {
+    //     let indexProject = new project("Index", 1);
+    //     function addTodo(title, description, duedate, priority, notes, checklist) {
+    //         indexProject.addTodo()
+    //     }
+    // }) ();
+    let defaultProject = new project();
+
     function addProject(name, priority, duedate) {
         let newProject = new project(name, priority, duedate);
         projects.push(newProject);
     };
 
-    return {addProject, projects};
+    return { addProject, addTodo: defaultProject.addTodo, projects };
 }) ();
 
 user.addProject("best world project", 1, "2011-10-1");
