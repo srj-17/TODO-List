f27f9647a10f785b07578ed5870f39decaee8df5
srj-17 <saugatrijal17@gmail.com>
2024-08-31 15:03:46 +0545

    Add functions to get and delete todos
    As we will need to show the todo information and
    delete todos later

--- src/index.js
+++ src/index.js
@@ -5,13 +5,6 @@ const user = (function () {
     let projects = [];
     
     // if todos added at random (outside of projects), they're added to default project
-    // it has no duedate since they're decided by the tasks
-    // let defaultProject = (function () {
-    //     let indexProject = new project("Index", 1);
-    //     function addTodo(title, description, duedate, priority, notes, checklist) {
-    //         indexProject.addTodo()
-    //     }
-    // }) ();
     let defaultProject = new project();
 
     function addProject(name, priority, duedate) {
@@ -19,7 +12,11 @@ const user = (function () {
         projects.push(newProject);
     };
 
-    return { addProject, addTodo: defaultProject.addTodo, projects };
+    function getProjects() {
+        return projects;
+    }
+
+    return { addProject, addTodo: defaultProject.addTodo, getProjects };
 }) ();
 
 user.addProject("best world project", 1, "2011-10-1");
--- src/project.js
+++ src/project.js
@@ -3,6 +3,8 @@ import todo from './todo';
 class Project {
     constructor(name, priority, dueDate) {
         this.name = name;
+
+        // project priority is just for sorting, don't want any complications here, with todos also having priroty
         this.priority = priority;
         this.dueDate = dueDate ? new Date(dueDate) : new Date();
         this.todo = [];
@@ -11,6 +13,17 @@ class Project {
     // we're trusting valid todo to be provided. should we? 
     addTodo(todo) {
         this.todo.push(todo);
+    }
+
+    getTodos() {
+        return this.todo;
+    }
+
+    // Id's corresponding to where the projects are kept in this.todo list
+    // as a consequence, the ids of all todos will be updated
+    deleteTodo(id) {
+        let index = id;
+        this.todo.splice(index, 1);
     }
 }
 
