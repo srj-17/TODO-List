b93fcaf5bde282045592497300781d9b7f2feb8e
srj-17 <saugatrijal17@gmail.com>
2024-08-31 17:09:43 +0545

    Make user.addTask() work using extra arrow funct.
    Before, we were exporting addTodo as:
    - { addTodo: directTodos.addTodo }, which means we
    were exporting the direct reference to addTodo object.
    - But exporting this means that access to the
    context (directTodos) is lost,
    - So, calling user.addTodo() wasn't recognizing the
    function at all.
    
    Solution: use an extra function that executes
    directTodos.addTodo() and export that instead. That way,
    we won't have to worry about context, the function is
    being executed inside the module, so it has access to
    directTodos.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import project from './project';
 const user = (function () {
     let projects = [];
     
-    // if todos added at random (outside of projects), they're added to default project (indexTodos)
-    let directTodos = new project();
+    // if todos added at random (outside of projects), they're added to default project (directTodos)
+    let directTodos = new project("Direct Todos", 1);
+    directTodos.addTodo("hello", "this is the todo of todods", "2011-10-1", 1, "ok these are notes for this project", ['hello', 'these', 'are', 'things', 'to', 'do'])
+    console.log(directTodos);
 
     function addProject(name, priority, duedate) {
         let newProject = new project(name, priority, duedate);
@@ -15,7 +17,13 @@ const user = (function () {
         return projects;
     }
 
-    return { addProject, addTodo: directTodos.addTodo, getProjects };
+    return { addProject, 
+             addTodo: () => directTodos.addTodo(), 
+             getDirectTodos: () => directTodos.getTodos(), 
+             getProjects };
 }) ();
 
 user.addProject("best world project", 1, "2011-10-1");
+console.log(user.getProjects());
+
+console.log(user.addTodo())
--- src/project.js
+++ src/project.js
@@ -7,23 +7,27 @@ class Project {
         // project priority is just for sorting, don't want any complications here, with todos also having priroty
         this.priority = priority;
         this.dueDate = dueDate ? new Date(dueDate) : new Date();
-        this.todo = [];
+        this.todoList = [];
     }
     
     // we're trusting valid todo to be provided. should we? YES Because we're creating a form in html # TODO
     addTodo(title, description, duedate, priority, notes, checklist) {
-        this.todo.push(new todo(title, description, duedate, priority, notes, checklist));
+        const newTodo = new todo(title, description, duedate, priority, notes, checklist);
+        this.todoList.push(newTodo);
+        console.log(this.todoList);
+        console.log(this.name);
+        console.log(this.priority)
     }
 
     getTodos() {
-        return this.todo;
+        return this.todoList;
     }
 
     // Id's corresponding to where the projects are kept in this.todo list
     // as a consequence, the ids of all todos will be updated
     deleteTodo(id) {
         let index = id;
-        this.todo.splice(index, 1);
+        this.todoList.splice(index, 1);
     }
 }
 
