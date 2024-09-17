bb12ffe3a43746208c6bc1c88e8ba1f6a3f32034
srj-17 <saugatrijal17@gmail.com>
2024-09-17 07:42:59 +0545

    Add basic structure to the html
    Only html, nothing done with css yet

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import user from './user';
+import './styles.css'
 
 user.addProject("best world project", 1, "2011-10-1");
 user.addDirectTodo("hello", "this is the todo of todods", "2011-10-1", 1, "ok these are notes for this project", ['hello', 'these', 'are', 'things', 'to', 'do'])
