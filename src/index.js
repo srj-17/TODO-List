8943500c4c5a3fd5386e26d0f68d22a4578c2f2f
srj-17 <saugatrijal17@gmail.com>
2024-09-18 17:07:35 +0545

    Expose some of the functions for todayTodo
    These are mapped directly from todo, so, might be
    similar to the directTodos implementation for similar
    effect in the past commits (not in current version of
    the app)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import user from './user';
 import './styles.css'
 
 user.addProject("best world project", 1, "2011-10-1");
-user.addDirectTodo("hello", "this is the todo of todods", "2011-10-1", 1, "ok these are notes for this project", ['hello', 'these', 'are', 'things', 'to', 'do'])
+user.addTodayTodo("hello", "this is the todo of todods", "2011-10-1", 1, "ok these are notes for this project", ['hello', 'these', 'are', 'things', 'to', 'do'])
 setTimeout(() => {
-    user.getDirectTodos().at(0).edit("bello", "this is the todo of todods", "2011-10-1", 1, "ok these are notes for this project", ['hello', 'these', 'are', 'things', 'to', 'do'])
-}, 3000);
-console.log(user.getDirectTodos());
+    user.getTodayTodos().at(0).edit("bello", "this is the todo of todods", "2011-10-1", 1, "ok these are notes for this project", ['hello', 'these', 'are', 'things', 'to', 'do'])
+}, 3000);   
+
+// you might see differences in output, because in chrome devTools, the output of list
+// changes based when you open it
+// In this case, if it is opened before 3 seconds, it shows "hello" as title, else bello
+console.log(user.getTodayTodos());
--- src/user.js
+++ src/user.js
@@ -3,17 +3,33 @@ import todo from './todo';
 
 const user = (function () {
     let projects = [];
-    let todayTodo = new Project("todayTodo", 1, new Date());
+    let todayTodo = new project("todayTodo", 1, new Date());
     
     // if todos added at random (outside of projects), they're added to today todos
     // create a new todayTodo with a new date, transfer all of yesterday's todos
     // to today's todayTodo 
-    // Do this each time DOM is loaded
+    // Do this each time DOM is loaded, or each time user log ins 
+    // [we don't have login system yet], so we do the first method
     function updateTodayTodo() {
         if (!(todayTodo.dueDate === new Date())) {
             todayTodo.changeDueDate(new Date());
+
+            // so that you don't have more than 20 items in your daily array
+            todayTodo.todoList = todayTodo.todoList.slice(0, 20);
         } 
     }
+
+    let addTodayTodo = (title, description, duedate, priority, notes, checklist) => {
+        return todayTodo.addTodo(title, description, duedate, priority, notes, checklist)
+    }
+
+    let editTodayTodo = (id, title, description, duedate, priority, notes, checklist) => {
+        return todayTodo.addTodo(id, title, description, duedate, priority, notes, checklist)
+    }
+
+    let getTodayTodos = () => todayTodo.getTodos();
+
+    let deleteTodayTodo = (id) => todayTodo.deleteTodo(id);
 
     function addProject(name, priority, duedate) {
         let newProject = new project(name, priority, duedate);
@@ -30,19 +46,13 @@ const user = (function () {
         projects.splice(index, 1);
     }
 
-    function addDirectTodo(title, description, duedate, priority, notes, checklist) {
-        let newTodo = new todo(title, description, duedate, priority, notes, checklist);
-        directTodos.push(newTodo);
-    }
-
-    function getDirectTodos() {
-        return directTodos;
-    }
-
     return { addProject, 
-             addDirectTodo,
-             getDirectTodos,
-             getProjects };
+        getProjects,
+        addTodayTodo,
+        editTodayTodo,
+        getTodayTodos,
+        deleteTodayTodo,
+    };
 }) ();
 
 export default user;
