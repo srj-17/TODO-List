56cc34ba293f6f99b7d323ddebca932522fe6854
srj-17 <saugatrijal17@gmail.com>
2025-01-02 11:02:36 +0545

    apply style changes + localstorage solve

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-(()=>{"use strict";const t=class{constructor(t,e,o){this.name=t,this.priority=e,this.dueDate=o?new Date(o):new Date,this.todo=[]}addTodo(t){this.todo.push(t)}};(function(){let e=[];return{addProject:function(o,s,r){let c=new t(o,s,r);e.push(c)},projects:e}})().addProject("best world project",1,"2011-10-1")})();
+(() => {
+  "use strict";
+  const t = class {
+    constructor(t, e, o) {
+      (this.name = t),
+        (this.priority = e),
+        (this.dueDate = o ? new Date(o) : new Date()),
+        (this.todo = []);
+    }
+    addTodo(t) {
+      this.todo.push(t);
+    }
+  };
+  (function () {
+    let e = [];
+    return {
+      addProject: function (o, s, r) {
+        let c = new t(o, s, r);
+        e.push(c);
+      },
+      projects: e,
+    };
+  })().addProject("best world project", 1, "2011-10-1");
+})();
 //# sourceMappingURL=main.js.map
